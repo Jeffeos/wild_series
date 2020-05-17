b176ede39cb4a84d3c4641b1ad5eeba90fea644f
Jeffeos <geoffrey.delenne@gmail.com>
2020-05-17 15:57:55 +0200

    added Show method to wild controller and view

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/", name="wild_index")
      */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @route("/show/{slug}",
+     *     requirements={"slug"="^[a-z\d\-]+$"},
+     *     defaults={"slug"= "Aucune série sélectionnée, veuillez choisir une série"},
+     *     name="wild_show")
+     */
+    public function show(string $slug) :Response
+    {
+        if ($slug != "Aucune série sélectionnée, veuillez choisir une série")
+        {
+            $slug = ucwords(str_replace('-', ' ', $slug));
+        }
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
     }
 }
