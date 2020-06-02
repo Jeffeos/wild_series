de567b083d39ba9bc0e897a733261b96f566a2a8
Jeffeos <geoffrey.delenne@gmail.com>
2020-06-02 16:14:13 +0200

    added user sign in, sign out

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+     private $passwordEncoder;
+
+     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+     {
+         $this->passwordEncoder = $passwordEncoder;
+     }
+
+    public function load(ObjectManager $manager)
+    {
+
+        // Création d’un utilisateur de type “auteur”
+        $subscriber = new User();
+        $subscriber->setEmail('subscriber@monsite.com');
+        $subscriber->setRoles(['ROLE_SUBSCRIBER']);
+        $subscriber->setPassword($this->passwordEncoder->encodePassword(
+            $subscriber,
+            'subscriberpassword'
+        ));
+
+        $manager->persist($subscriber);
+
+        // Création d’un utilisateur de type “administrateur”
+        $admin = new User();
+        $admin->setEmail('admin@monsite.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+            $admin,
+            'adminpassword'
+        ));
+
+        $manager->persist($admin);
+
+        // Sauvegarde des 2 nouveaux utilisateurs :
+        $manager->flush();
+    }
+}
